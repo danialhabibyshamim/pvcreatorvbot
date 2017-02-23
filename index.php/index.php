f1b5ad78662e730f18311995721784314ace87e0
danialhabibyshamim <hasanhacker@chmail.ir>
2017-02-23 13:44:03 +0330

    index.php
    danial

---
+++ index.php/index.php
@@ -0,0 +1,22 @@
+# pvcreatorvt<?php
+define('API_KEY','token');
+
+function worldphp($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+
+$update = json_decode(file_get_contents('php://input'));
+$text = $update->message->text;
+$chat_id = $update->message->chat->id;
+$message_id = $update->message->message_id;
+$admin = 266151360;
